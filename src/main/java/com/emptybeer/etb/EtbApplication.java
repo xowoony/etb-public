2e843e98deac8a50e076293ca69439be2691792d
xowoony <trumanblack@hanmail.net>
2022-12-08 20:07:36 +0900

    test

---
+++ src/main/java/com/emptybeer/etb/EtbApplication.java
@@ -0,0 +1,13 @@
+package com.emptybeer.etb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EtbApplication.class, args);
+    }
+
+}
