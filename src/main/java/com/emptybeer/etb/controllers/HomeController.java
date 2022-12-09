519140299deba0545d17b1264ff9efa45104bc01
xowoony <trumanblack@hanmail.net>
2022-12-10 00:07:46 +0900

    해결한 html+css

--- src/main/java/com/emptybeer/etb/controllers/HomeController.java
+++ src/main/java/com/emptybeer/etb/controllers/HomeController.java
@@ -6,17 +6,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-@Controller(value="com.emptybeer.etb.controllers.HomeController")
-@RequestMapping(value="/")
+@Controller(value = "com.emptybeer.etb.controllers.HomeController")
+@RequestMapping(value = "/")
 public class HomeController {
-    @GetMapping(value="/",
-    produces = MediaType.TEXT_HTML_VALUE)
+    @GetMapping(value = "/",
+            produces = MediaType.TEXT_HTML_VALUE)
     public ModelAndView getIndex() {
         ModelAndView modelAndView = new ModelAndView("home/index");
         return modelAndView;
     }
 
-    @GetMapping(value="product",
+    @GetMapping(value = "product",
             produces = MediaType.TEXT_HTML_VALUE)
     public ModelAndView getProduct() {
         ModelAndView modelAndView = new ModelAndView("home/product");
@@ -24,14 +24,14 @@ public class HomeController {
     }
 
 
-    @GetMapping(value="community",
+    @GetMapping(value = "community",
             produces = MediaType.TEXT_HTML_VALUE)
     public ModelAndView getCommunity() {
         ModelAndView modelAndView = new ModelAndView("home/community");
         return modelAndView;
     }
 
-    @GetMapping(value="festival",
+    @GetMapping(value = "festival",
             produces = MediaType.TEXT_HTML_VALUE)
     public ModelAndView getFestival() {
         ModelAndView modelAndView = new ModelAndView("home/festival");
