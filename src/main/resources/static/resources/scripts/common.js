827a4de84474609708e546005163c0dc20a5c4e1
xowoony <trumanblack@hanmail.net>
2022-12-10 11:28:29 +0900

    해결한 html+css

---
+++ src/main/resources/static/resources/scripts/common.js
@@ -0,0 +1,10 @@
+const Cover = {
+    show: (text) => {
+        const cover = window.document.getElementById('cover');
+        cover.querySelector('[rel="text"]').innerText = text;
+        cover.classList.add('visible');
+    },
+    hide: () => {
+        window.document.getElementById('cover').classList.remove('visible');
+    }
+};
