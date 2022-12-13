2ac01171c41f9cd713e45b26587f54e8b1275476
riverwings <candylove95@naver.com>
2022-12-13 20:50:34 +0900

    리뷰 아티클 백엔드 js 작업중 getReviewWrite, postReviewWrite 만듦

---
+++ src/main/java/com/emptybeer/etb/enums/bbs/WriteResult.java
@@ -0,0 +1,8 @@
+package com.emptybeer.etb.enums.bbs;
+
+import com.emptybeer.etb.interfaces.IResult;
+
+public enum WriteResult implements IResult {
+    NOT_ALLOWED,
+    NO_SUCH_BOARD
+}
--- src/main/java/com/emptybeer/etb/services/BbsService.java
+++ src/main/java/com/emptybeer/etb/services/BbsService.java
@@ -1,7 +1,35 @@
 package com.emptybeer.etb.services;
 
+import com.emptybeer.etb.entities.bbs.BoardEntity;
+import com.emptybeer.etb.entities.bbs.ReviewArticleEntity;
+import com.emptybeer.etb.enums.CommonResult;
+import com.emptybeer.etb.enums.bbs.WriteResult;
+import com.emptybeer.etb.interfaces.IResult;
+import com.emptybeer.etb.mappers.IBbsMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service(value = "com.emptybeer.etb.services.BbsServices")
 public class BbsService {
+    private final IBbsMapper bbsMapper;
+
+    @Autowired
+    public BbsService(IBbsMapper bbsMapper) {
+        this.bbsMapper = bbsMapper;
+    }
+
+    public BoardEntity getBoard(String id) {
+        return this.bbsMapper.selectBoardById(id);
+    }
+
+    public Enum<? extends IResult> reviewWrite(ReviewArticleEntity reviewArticle) {
+        BoardEntity board = this.bbsMapper.selectBoardById(reviewArticle.getBoardId());
+
+        if (board == null) {
+            return WriteResult.NO_SUCH_BOARD;
+        }
+        return this.bbsMapper.insertReviewArticle(reviewArticle) > 0
+                ? CommonResult.SUCCESS
+                : CommonResult.FAILURE;
+    }
 }
