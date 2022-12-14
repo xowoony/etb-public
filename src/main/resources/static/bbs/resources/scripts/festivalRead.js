4adb2c23023f4570d5d5101bd8a9fbaec8e38a52
HEZAE_SPECTRE\HP <ilhojung92@gmail.com>
2022-12-14 12:51:08 +0900

    Update FestivalRead.html, css, js
    festivalRead 관련 front작업 진행
    맵 추가

---
+++ src/main/resources/static/bbs/resources/scripts/festivalRead.js
@@ -0,0 +1,9 @@
+var container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
+
+
+var options = { //지도를 생성할 때 필요한 기본 옵션
+    center: new kakao.maps.LatLng(35.85086, 128.558785), //지도의 중심좌표.
+    level: 3 //지도의 레벨(확대, 축소 정도)
+};
+
+var map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
