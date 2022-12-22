c6f6489603ad69637b033a47dc09d76ca188f585
xowoony <trumanblack@hanmail.net>
2022-12-22 10:50:36 +0900

    BEER 맵핑

--- src/main/java/com/emptybeer/etb/controllers/DataController.java
+++ src/main/java/com/emptybeer/etb/controllers/DataController.java
@@ -1,4 +1,29 @@
 package com.emptybeer.etb.controllers;
 
+import com.emptybeer.etb.services.DataService;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping(value = "/data")
 public class DataController {
+    // 의존성 주입
+    private final DataService dataService;
+
+    public DataController(DataService dataService) {
+        this.dataService = dataService;
+    }
+
+    @GetMapping(value = "beer",
+            produces = MediaType.TEXT_HTML_VALUE)
+    public ModelAndView getBeer() {
+        ModelAndView modelAndView = new ModelAndView("data/beer");
+        return modelAndView;
+    }
+
+
+
 }
