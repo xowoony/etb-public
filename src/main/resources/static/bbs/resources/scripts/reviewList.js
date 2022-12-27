d661d7f137e157f007a01e5e0e767ad34aae6259
riverwings <candylove95@naver.com>
2022-12-27 18:59:10 +0900

    리뷰 리스트 만드는중 맥주좋아요(새로고침 없이 로드 되는것으로 수정 필요), 검색기능 구현중

--- src/main/resources/static/bbs/resources/scripts/reviewList.js
+++ src/main/resources/static/bbs/resources/scripts/reviewList.js
@@ -54,4 +54,57 @@ if (!likeToggleElement.classList.contains('prohibited')) {
     });
 }
 
+// 본인글 수정 삭제 메뉴
+const menuIcon = document.getElementById('menuIcon');
+const xButton = document.getElementById('xButton');
+if (menuIcon !== null) {
+    menuIcon.addEventListener('click', () => {
+        document.getElementById('menuBox').classList.add('visible');
+    });
 
+    xButton.addEventListener('click', () => {
+        document.getElementById('menuBox').classList.remove('visible');
+    });
+}
+
+
+// 리뷰 삭제
+const reviewListForm = document.getElementById('reviewListForm');
+const deleteButton = document.getElementById('deleteButton');
+if (deleteButton !== null) {
+    deleteButton.addEventListener('click', () => {
+        if (!confirm('정말로 게시글을 삭제할까요?')) {
+            return;
+        }
+        const xhr = new XMLHttpRequest();
+        const formData = new FormData();
+        formData.append('aid', reviewListForm['aid'].value);
+
+        xhr.open('DELETE', "./reviewRead");
+        xhr.onreadystatechange = () => {
+            if (xhr.readyState === XMLHttpRequest.DONE) {
+                if (xhr.status >= 200 && xhr.status < 300) {
+                    const responseObject = JSON.parse(xhr.responseText);
+                    switch (responseObject['result']) {
+                        case 'success':
+                            const beerIndex = reviewListForm['beerIndex'].value;
+                            window.location.href = `reviewList?beerIndex=${beerIndex}`;
+                            alert('게시글이 삭제되었습니다.');
+                            break;
+                        case 'no_such_article':
+                            alert('삭제하려는 게시글이 더 이상 존재하지 않습니다.\n\n이미 삭제되었을 수도 있습니다.');
+                            break
+                        case 'not_allowed':
+                            alert('해당 게시글을 삭제할 권한이 없습니다.');
+                            break;
+                        default:
+                            alert('알 수 없는 이유로 게시글을 삭제하지 못하였습니다.\n\n잠시 후 다시 시도해 주세요.');
+                    }
+                } else {
+                    alert('서버와 통신하지 못하였습니다.\n\n잠시 후 다시 시도해 주세요.');
+                }
+            }
+        };
+        xhr.send(formData);
+    });
+}
