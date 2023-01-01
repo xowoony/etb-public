955531872e1892752d8e8ab70326aefd4e309a81
xowoony <trumanblack@hanmail.net>
2023-01-01 17:11:46 +0900

    index html 섹션2 이미지 슬라이드

--- src/main/java/com/emptybeer/etb/mappers/IDataMapper.java
+++ src/main/java/com/emptybeer/etb/mappers/IDataMapper.java
@@ -1,7 +1,13 @@
 package com.emptybeer.etb.mappers;
 
+import com.emptybeer.etb.entities.data.BeerEntity;
+import com.emptybeer.etb.vos.BeerVo;
+import com.emptybeer.etb.vos.ReviewArticleVo;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 @Mapper
 public interface IDataMapper {
+//    BeerEntity selectBeerImageByIndex(@Param(value = "index") int index);
+//
 }
--- src/main/java/com/emptybeer/etb/services/DataService.java
+++ src/main/java/com/emptybeer/etb/services/DataService.java
@@ -1,19 +1,29 @@
 package com.emptybeer.etb.services;
 
 import com.emptybeer.etb.entities.data.BeerEntity;
+import com.emptybeer.etb.entities.member.UserEntity;
 import com.emptybeer.etb.mappers.IDataMapper;
+import com.emptybeer.etb.models.PagingModel;
+import com.emptybeer.etb.vos.BeerVo;
+import com.emptybeer.etb.vos.ReviewArticleVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service(value = "com.emptybeer.etb.services.DataService")
 public class DataService {
 
-    private final IDataMapper iDataMapper;
+    private final IDataMapper dataMapper;
 
     @Autowired
-    public DataService(IDataMapper iDataMapper) {
-        this.iDataMapper = iDataMapper;
+    public DataService(IDataMapper dataMapper) {
+        this.dataMapper = dataMapper;
     }
+
+    // 맥주 이미지, 이름 가져오기
+//    public BeerEntity getBeer(int index) {
+//        return this.dataMapper.selectBeerImageByIndex(index);
+//    }
+
 
 
 }
