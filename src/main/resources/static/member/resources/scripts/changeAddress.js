17d430c5dfdebe02c2dbe41c43d46059ad3de072
xowoony <trumanblack@hanmail.net>
2023-01-10 17:39:51 +0900

    1. 카카오 주소찾기 API 연동완료 2. 주소 변경 백엔드는 아직 구현 중

---
+++ src/main/resources/static/member/resources/scripts/changeAddress.js
@@ -0,0 +1,34 @@
+const form = window.document.getElementById('form');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 우편번호 찾기 기능
+form['addressFind'].addEventListener('click', () => {
+    new daum.Postcode({
+        oncomplete: e => {
+            form.querySelector('[rel="addressFindPanel"]').classList.remove('visible');
+            form['addressPostal'].value = e['zonecode'];
+            form['addressPrimary'].value = e['address'];
+            form['addressSecondary'].value = '';
+            form['addressSecondary'].focus();
+        }
+    }).embed(form.querySelector('[rel="addressFindPanelDialog"]'));
+    form.querySelector('[rel="addressFindPanel"]').classList.add('visible');
+});
+
+form.querySelector('[rel="addressFindPanel"]').addEventListener('click', () => {
+    form.querySelector('[rel="addressFindPanel"]').classList.remove('visible');
+});
