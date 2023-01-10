fbe4840fd428c8b10c4ebec961f8afc927ba38b6
riverwings <candylove95@naver.com>
2023-01-10 13:37:58 +0900

    마이페이지 조회 기능까지 구현. 전체 삭제 및 좋아요 취소등 기능 구현 필요

--- src/main/java/com/emptybeer/etb/vos/BasicCommentVo.java
+++ src/main/java/com/emptybeer/etb/vos/BasicCommentVo.java
@@ -7,6 +7,18 @@ public class BasicCommentVo extends BasicCommentEntity {
     private String userNickname;
     private boolean isSigned;   // 로그인을 했는가에 대한 여부
     private boolean isMine;
+    private String articleTitle;
+
+    private String boardName;
+
+    public String getArticleTitle() {
+        return articleTitle;
+    }
+
+    public BasicCommentVo setArticleTitle(String articleTitle) {
+        this.articleTitle = articleTitle;
+        return this;
+    }
 
     public String getUserNickname() {
         return userNickname;
@@ -32,6 +44,15 @@ public class BasicCommentVo extends BasicCommentEntity {
 
     public BasicCommentVo setMine(boolean mine) {
         isMine = mine;
+        return this;
+    }
+
+    public String getBoardName() {
+        return boardName;
+    }
+
+    public BasicCommentVo setBoardName(String boardName) {
+        this.boardName = boardName;
         return this;
     }
 }
--- src/main/resources/static/memberBbs/resources/scripts/myLike.js
+++ src/main/resources/static/memberBbs/resources/scripts/myLike.js
@@ -1,10 +1,10 @@
 function checkSelectAll()  {
     // 전체 체크박스
     const checkboxes
-        = document.querySelectorAll('input[name="myLike"]');
+        = document.querySelectorAll('input[name="myArticle"]');
     // 선택된 체크박스
     const checked
-        = document.querySelectorAll('input[name="myLike"]:checked');
+        = document.querySelectorAll('input[name="myArticle"]:checked');
     // select all 체크박스
     const selectAll
         = document.querySelector('input[name="selectall"]');
@@ -19,7 +19,36 @@ function checkSelectAll()  {
 
 function selectAll(selectAll)  {
     const checkboxes
-        = document.getElementsByName('myLike');
+        = document.getElementsByName('myArticle');
+
+    checkboxes.forEach((checkbox) => {
+        checkbox.checked = selectAll.checked
+    })
+}
+
+
+function checkSelectAll2()  {
+    // 전체 체크박스
+    const checkboxes
+        = document.querySelectorAll('input[name="myReview"]');
+    // 선택된 체크박스
+    const checked
+        = document.querySelectorAll('input[name="myReview"]:checked');
+    // select all 체크박스
+    const selectAll
+        = document.querySelector('input[name="selectall2"]');
+
+    if(checkboxes.length === checked.length)  {
+        selectAll.checked = true;
+    }else {
+        selectAll.checked = false;
+    }
+
+}
+
+function selectAll2(selectAll)  {
+    const checkboxes
+        = document.getElementsByName('myReview');
 
     checkboxes.forEach((checkbox) => {
         checkbox.checked = selectAll.checked
