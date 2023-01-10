4d9f6cb1b6b22aab3e5228110e4fcdd941048a96
riverwings <candylove95@naver.com>
2023-01-10 13:37:58 +0900

    마이페이지 조회 기능까지 구현. 전체 삭제 및 좋아요 취소등 기능 구현 필요

--- src/main/java/com/emptybeer/etb/mappers/IMemberBbsMapper.java
+++ src/main/java/com/emptybeer/etb/mappers/IMemberBbsMapper.java
@@ -1,8 +1,9 @@
 package com.emptybeer.etb.mappers;
 
+import com.emptybeer.etb.entities.bbs.BasicArticleLikeEntity;
+import com.emptybeer.etb.entities.bbs.ReviewArticleLikeEntity;
 import com.emptybeer.etb.entities.member.UserEntity;
-import com.emptybeer.etb.vos.BasicArticleVo;
-import com.emptybeer.etb.vos.ReviewArticleVo;
+import com.emptybeer.etb.vos.*;
 import org.apache.catalina.User;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
@@ -15,4 +16,10 @@ public interface IMemberBbsMapper {
 
     // 작성 리뷰 리스트
     ReviewArticleVo[] selectReviewsByEmail(@Param(value = "email") String email);
+
+    // 작성 댓글 리스트
+    BasicCommentVo[] selectCommentsByEmail(@Param(value = "email") String email);
+
+    // 좋아요 한 기본 게시글 리스트
+    BasicArticleLikeVo[] selectArticlesByLike(@Param(value = "email") String email);
 }
---
+++ src/main/java/com/emptybeer/etb/vos/ReviewArticleLikeVo.java
@@ -0,0 +1,57 @@
+package com.emptybeer.etb.vos;
+
+import com.emptybeer.etb.entities.bbs.ReviewArticleLikeEntity;
+
+public class ReviewArticleLikeVo extends ReviewArticleLikeEntity {
+    private String title;
+    private String contentGood;
+    private String contentBad;
+    private String userNickname;
+
+    private String beerName;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public ReviewArticleLikeVo setTitle(String title) {
+        this.title = title;
+        return this;
+    }
+
+    public String getContentGood() {
+        return contentGood;
+    }
+
+    public ReviewArticleLikeVo setContentGood(String contentGood) {
+        this.contentGood = contentGood;
+        return this;
+    }
+
+    public String getContentBad() {
+        return contentBad;
+    }
+
+    public ReviewArticleLikeVo setContentBad(String contentBad) {
+        this.contentBad = contentBad;
+        return this;
+    }
+
+    public String getUserNickname() {
+        return userNickname;
+    }
+
+    public ReviewArticleLikeVo setUserNickname(String userNickname) {
+        this.userNickname = userNickname;
+        return this;
+    }
+
+    public String getBeerName() {
+        return beerName;
+    }
+
+    public ReviewArticleLikeVo setBeerName(String beerName) {
+        this.beerName = beerName;
+        return this;
+    }
+}
